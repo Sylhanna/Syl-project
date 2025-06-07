78b6dd54d73546e4cb42632bb0fd8640a00638e0
Sylhanna <sylhanna.rodriguez@gmail.com>
2025-06-07 14:55:18 +0800

    Calculator
    my first python project

---
+++ py projects/python.py
@@ -0,0 +1,19 @@
+
+firstNumber = float(input("Enter a First Number : "))
+secondNumber = float(input("Enter a Second Number : "))
+
+sum = firstNumber + secondNumber
+
+print("the sum of ", str(firstNumber) + " + " + str(secondNumber) + " is equal to " + str(sum))
+
+difference = firstNumber - secondNumber
+
+print("the difference of ", str(firstNumber) + " - " + str(secondNumber) + " is equal to " + str(difference))
+
+product = firstNumber * secondNumber
+
+print("the product of ", str(firstNumber) + " * " + str(secondNumber) + " is equal to " + str(product))
+
+quotient = firstNumber / secondNumber
+
+print("the quotient of ", str(firstNumber) + " / " + str(secondNumber) + " is equal to " + str(quotient))
